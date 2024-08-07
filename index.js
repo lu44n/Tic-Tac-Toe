c17545c813847bcc9b722a4cee8648240f1c1709
Mikael <99888430+lu44n@users.noreply.github.com>
2024-08-07 15:50:16 -0300

    Finished
    Todas funcionalidades implementadas:
    
    - Incluir os nomes dos dois jogadores;
    - O nome do jogador da vez deve ser mostrado na tela e alterado a medida que os turnos vão se alternando;
    - Um tabuleiro deve ser mostrado na tela e ser atualizado quando o jogador clicar na região que ele quer marcar;
    - Quando um jogador clicar no tabuleiro deve ser marcado um “X” ou “O” de acordo com o jogador da vez e não deve ser possível clicar naquela região novamente;
    - Quando um jogador ganhar seu nome deve ser mostrado na tela e as regiões da vitória devem ser destacadas de alguma forma;
    - Em caso de empate, uma mensagem de empate deve ser mostrada na tela;
    - Deve ser possível reiniciar o jogo para jogar novamente.

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-|
+const player1 = prompt('Digite o nome do primeiro jogador: ')
+const player2 = prompt('Digite o nome do segundo jogador: ')
+
+const x_player = 'X'
+const o_player = 'O'
+
+const userWhoPlays = document.getElementById('userWhoPlays')
+userWhoPlays.innerText += ' ' + player1
+
+let list_plays = []
+let player1_played = []
+let player2_played = []
+const possible_wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+function checkWin(possibleWins, playerMoves) {
+    // Função para verificar se todos os elementos de um array estão em outro array
+    function isSubset(subset, set) {
+        return subset.every(val => set.includes(val));
+    }
+    // Verificar cada possível combinação vencedora
+    for (let win of possibleWins) {
+        if (isSubset(win, playerMoves)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+document.querySelectorAll('.cell').forEach(function (cellDiv){
+    cellDiv.addEventListener('click', function (ev){
+        const cellbttn = ev.currentTarget
+        if (list_plays.length === 0){
+            cellbttn.innerText = x_player
+            list_plays.push(x_player)
+            player1_played.push(parseInt(cellbttn.dataset.cell))
+            // console.log(player1_played)
+            userWhoPlays.innerText = 'Jogador da vez: ' + player2
+        } else if (list_plays[list_plays.length - 1] === x_player) {
+            cellbttn.innerText = o_player
+            list_plays.push(o_player)
+            player2_played.push(parseInt(cellbttn.dataset.cell))
+            const checkIfPlayer2Wins = checkWin(possible_wins, player2_played)
+            if (checkIfPlayer2Wins === true){
+                alert("O vencedor foi o " + player2 + " !")
+            }
+            // console.log(player2_played)
+            userWhoPlays.innerText = 'Jogador da vez: ' + player1
+        } else if (list_plays[list_plays.length - 1] === o_player) {
+            cellbttn.innerText = x_player
+            list_plays.push(x_player)
+            player1_played.push(parseInt(cellbttn.dataset.cell))
+            checkIfPlayer1Wins = checkWin(possible_wins, player1_played)
+            if (checkIfPlayer1Wins === true){
+                alert("O vencedor foi o " + player1 + " !")
+            } else if (player1_played.length + player2_played.length === 9){
+                alert("Empate! Toque em reiniciar para jogar novamente!")
+            }
+            // console.log(player1_played)
+            userWhoPlays.innerText = 'Jogador da vez: ' + player2
+        }
+    })
+})
+
+document.getElementById('restartBttn').addEventListener('click', function (){
+    document.querySelectorAll('.cell').forEach(function (cell){
+        cell.innerText = ''
+    })
+    list_plays = []
+    player1_played = []
+    player2_played = []
+    userWhoPlays.innerText = 'Jogador da vez: '+ player1
+})
